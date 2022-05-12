8c2d1b7d66fcfbba1409eba6d369a70abe0bda82
rodriguezandresed <rodriguezandresed@gmail.com>
2022-05-12 19:31:46 -0400

    [starting point log in]

--- src/main.py
+++ src/main.py
@@ -5,16 +5,19 @@ import os
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, AdminUser, TipoServicio, EvaluacionProveedor, OrdenServicio 
 #from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["JWT_SECRET_KEY"]=os.environ.get("horse cat mouse")
+jwt = JWTManager(app)
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
@@ -30,14 +33,114 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/users', methods=['GET'])
+@app.route('/user/<int:user_id>', methods=['GET', 'PUT', 'DELETE'])
+def handle_user(user_id = None):
+	if request.method == 'GET':
+		if user_id  is None:
+			users = User.query.all()
+			users = list(map(lambda user: user.serialize(), users))
+			return jsonify(users),200
+		else:
+			user = User.query.filter_by(id=user_id).first()
+			if user is not None:
+				return jsonify(user.serialize()),200
+			else:
+				return jsonify({
+					"msg": "user not found"
+				}), 404
+	if request.method == 'PUT':
+		body = request.json
+		if not body.get("email"):
+			return jsonify({
+				"msg": "something happened, try again"				
+			}), 400
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+		if not body.get("password"):
+			return jsonify({
+				"msg": "something happened, try again"				
+			}), 400
+			
+		user_update = User.query.filter_by(id=user_id).first()
+		
+		if user_update is None:
+			return jsonify({
+				"msg": "User not found"
+			}), 404
 
-    return jsonify(response_body), 200
+		user = User(email=body["email"], password=body["password"])	
+		try:
+		
+			user_update.email = body.get("email")
+			db.session.commit()
+			return jsonify(user.serialize()), 201
+		except Exception as error:
+			db.session.rollback()
+			return jsonify(error.args)
+		
+		
+	if request.method == 'DELETE':		
+		user_delete = User.query.filter_by(id=user_id).first()
+		if user_delete is None:
+			return jsonify({
+				"msg": "User not found"
+			}), 404
+			
+		db.session.delete(user_delete)
+		
+		try: 
+			db.session.commit()
+			return jsonify([]), 204
+		except Exception as error:
+			db.session.rollback()
+			return jsonify(error.args)
+
+@app.route('/user', methods=['POST'])
+def handle_other_user():
+	body = request.json
+	
+	if not body.get("email"):
+		return jsonify({
+			"msg": "something happened, try again"
+		}), 400
+		
+	user = User(email=body["email"], password=body["password"])
+	try:
+		db.session.add(user)
+		db.session.commit()
+		return jsonify(user.serialize()), 201
+	
+	except Exception as error:
+		db.session.rollback()
+		return jsonify(error.args), 500
+		
+@app.route('/login', methods=['POST'])
+def handle_login():
+	email=request.json.get("email", None)
+	password=request.json.get("password", None)
+
+	if email is not  None and password is not None:
+		user = User.query.filter_by(email=email, password=password).one_or_none()
+		if user is not None:
+			print(user.id)
+			create_token=create_access_token(identity=user.id)
+			return jsonify(
+				{
+					"token":create_token,
+					"user_id":user.id,
+					"email":user.email
+
+				}
+			)
+		else:
+			return jsonify({
+			"msg": "Not Found"
+		}), 404
+
+	else:
+		return jsonify({
+			"msg": "something happened, try again"
+		}), 400	
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
